81178cd600d9d0b1ed30d7c8966b73350bd557b5
Srilekha Paul <srilekhapaul2003@gmail.com>
2025-09-01 13:32:13 +0530

    create 5 jokes making

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,41 @@ app.get('/', (req, res) =>{
   }
 );
 
+
+//get a list of 5 jokes
+app.get('/jokes', (req, res) =>{
+
+    const jokes = [
+        {
+            id:1,
+            title: 'A joke',
+            content: 'This is a joke'
+
+        },
+        {
+            id:2,
+            title: 'Another joke',
+            content: 'This is another joke'
+        },
+        {
+            id:3,
+            title: 'A third joke',
+            content: 'This is third joke'
+        },
+        {
+            id:4,
+            title: 'A fourth joke',
+            content: 'This is fourth joke'
+        },
+        {
+            id:5,
+            title: 'A fith joke',
+            content: 'This is fifth joke'
+        }
+    ]
+    res.send('jokes');
+   }
+);
 const port = process.env.PORT  || 3000;
 
 app.listen(port, () => {
