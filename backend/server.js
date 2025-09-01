1b2f1b20f6b22fc97b89a5fb1b3c0eca6a591618
Srilekha Paul <srilekhapaul2003@gmail.com>
2025-09-01 13:17:55 +0530

    solved error

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
-const express  = require('express');
-
 import   express from 'express';
 
-const app
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('Server is ready');
+  }
+);
+
+const port = process.env.PORT  || 3000;
+
+app.listen(port, () => {
+    console.log(`serve at http://localhost:${port}`);
+  }
+);
